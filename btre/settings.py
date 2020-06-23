262d900580b52a2c4fbf56dee4687e37b13d97e8
Arman Kassenov <arman7su@gmail.com>
2020-06-23 10:20:01 +0600

    Added Local Settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'abylaikassenov7@gmail.com'
 EMAIL_HOST_PASSWORD = 'Tamos777,'
 EMAIL_USE_TLS=True
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
